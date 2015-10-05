4c01da904fe062bbf6d9fd265e4a27a56deaaaf2
Wyland Harris <wyland.harris@gmail.com>
2015-10-05 06:06:38 -0500

    Challenge0

---
+++ src/harris_4_primes/Harris_3_challenge0.java
@@ -0,0 +1,19 @@
+package harris_4_primes;
+
+public class Harris_3_challenge0 {
+static boolean isNotDivisable(int someNumber){
+    boolean prime = true;
+    int divisors =0;
+    for (int i = 2; i<someNumber/2; i++){
+        if (someNumber %i==0){
+            prime = false;    
+    return ("Davis");
+    
+    static boolean isDivisable(int someNumber){
+    boolean prime = true;
+    int divisors =0;
+    for (int i = 2; i<someNumber/2; i++){
+        if (someNumber %i==0){
+            prime = false;    
+    return ("Ninja");
+}
