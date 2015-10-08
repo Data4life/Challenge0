9bf4ee9d34c88311dd9f36030ec0bd520ae77428
Wyland Harris <Data4life@users.noreply.github.com>
2015-10-08 11:01:54 -0500

    Update Harris_3_challenge0.java

--- src/harris_4_primes/Harris_3_challenge0.java
+++ src/harris_4_primes/Harris_3_challenge0.java
@@ -1,19 +1,46 @@
-package harris_4_primes;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harris_3_challenge0;
 
+/**
+ *
+ * @author Data4Life
+ */
 public class Harris_3_challenge0 {
-static boolean isNotDivisable(int someNumber){
-    boolean prime = true;
-    int divisors =0;
-    for (int i = 2; i<someNumber/2; i++){
-        if (someNumber %i==0){
-            prime = false;    
-    return ("Davis");
-    
-    static boolean isDivisable(int someNumber){
-    boolean prime = true;
-    int divisors =0;
-    for (int i = 2; i<someNumber/2; i++){
-        if (someNumber %i==0){
-            prime = false;    
-    return ("Ninja");
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            int evens = 0;
+        for(int i =0; i<500; i++){
+            if (isDivisblebyfive(i) && isDivisblebyeleven(i)){
+                if (isDivisblebyfive(i)){
+                    System.out.println("Animate this");
+                }
+            }else if (isDivisblebythree(i) & isDivisblebyeleven(i)){
+                System.out.println("Herr Direktor");
+            }
+            else{
+                System.out.println(i);
+            }
+        }    
+    }  
+
+
+static boolean isDivisblebytwo(int someNumber) {
+    return (someNumber % 2 == 0);
+}
+static boolean isDivisblebythree(int someNumber) {
+    return (someNumber % 3 == 0);
+}
+static boolean isDivisblebyfive(int someNumber) {
+    return (someNumber % 5 == 0);
+}
+static boolean isDivisblebyeleven(int someNumber) {
+    return (someNumber % 11 == 0);
+}
 }
